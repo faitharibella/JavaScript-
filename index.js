b88e70885a874b4d9ed716467526b983d1bd7b88
faitharibella <faithahaisibwe730@gmail.com>
2026-03-16 10:34:58 +0300

    first javascript file

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// this line of code prints out strings
+console.log("hello world", "this is the class of march 2026");
+
+console.log(`this is a back tick`);
+
+console.log(23);
+
+console.log(true)
+
+console.log(false)
