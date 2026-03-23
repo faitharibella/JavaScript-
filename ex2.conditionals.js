44b6ac19e8c3437dab1ab838484bdb407ec20b47
faitharibella <faithahaisibwe730@gmail.com>
2026-03-23 18:22:54 +0300

    Added the conditional assignment

---
+++ ex2.conditionals.js
@@ -0,0 +1,9 @@
+let buyerName = "Ari" 
+let amountDue =20000
+let itemName = "cement" 
+if (buyerName.length>=2 && amountDue >= 10000 && itemName.length >0)
+    { 
+   console.log("The record is valid")
+}else{
+    console.log("The record is invalid")
+}
