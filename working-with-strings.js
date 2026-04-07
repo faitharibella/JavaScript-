d74fd7ec0f70c3557e51420b45240082e860937b
faitharibella <faithahaisibwe730@gmail.com>
2026-04-07 17:06:57 +0300

    added my strings class practice

---
+++ working-with-strings.js
@@ -0,0 +1,61 @@
+// let firstName= "Faith"
+// let lastName= "Ahaisibwe"
+
+//  let fullName= firstName + " " + lastName
+
+
+
+
+// // let fullName = `This is the well formatted full name: ${firstName} ${lastName}`
+
+//  console.log(fullName)
+
+// // console.log(`The length of the full name is ${(fullName.length)}`)
+
+// // let phoneNumber = "0756789156"
+
+// // console.log(phoneNumber.length)
+
+// // getting initials of the user
+
+// // let initials= `${firstName[0]}.${lastName[0]}`
+// // console.log(initials)
+
+// // console.log("This is uppercase to all characters",firstName.toUpperCase())
+
+// // console.log("This is uppercase to all characters",firstName.toLowerCase())
+
+// let transactionId= "FN362828CH67"
+
+// let NIN = "CM123H37565"
+// let _NIN = "CF364783384"
+
+// // console.log(NIN.substring(0,2))
+// // console.log(_NIN.substring(0,2))
+
+// Faith Ahaisibwe
+
+// console.log(fullName.split(" "))
+
+// let email = "fahaisibwe@student.refactory.academy"
+
+// console.log(email.split("@"))
+
+// console.log(firstName.at(0))
+
+// let password = "27890 " //intended to write without the space
+
+// console.log(password)
+// console.log(password.trim(),"|")
+
+let favouriteSong = "Dynamite"
+
+// console.log("My favourite song is", favouriteSong)
+
+let fullName = "Faith"
+
+console.log(fullName[0])
+
+console.log(fullName.length >=4)
+
+console.log(fullName.toUpperCase())
