fc81676decc35bcd76647ea71369b7380700535a
faitharibella <faithahaisibwe730@gmail.com>
2026-04-07 16:46:33 +0300

    booleons prac

---
+++ boolean.js
@@ -0,0 +1,67 @@
+// // loose equality
+
+// let amount = "1000";
+// let amountFromDB = 5000;
+
+// // console.log(amount == amountFromDB)
+
+// // strict equality operator
+
+// // console.log(amount === amountFromDB)
+
+// let userCredentials = null;
+
+// // console.log(userCredentials === null)
+
+// // loose inequality !=
+
+// // console.log(amount != amountFromDB)
+
+// // strict inequality !==
+
+// //  console.log (amount !== amountFromDB)
+
+// // greater operator >
+
+// //  console.log(amount > amountFromDB)
+
+// let reportedAge = 33;
+// let actualAge = 33;
+
+// let gender = "M";
+
+// let isAMan = reportedAge >= 18 && gender === "M";
+
+// // console.log(isAMan)
+
+// let isAPublicHoliday = true;
+// let isAWeekend = false;
+
+// // console.log(isAPublicHoliday || isAWeekend);
+
+// // console.log(gender === "M" && reportedAge>= 18) || (isAPublicHoliday || isAWeekend)
+
+// let isNotLoggedIn = !userCredentials
+
+// // console.log (isNotLoggedIn)
+
+// let stockQuantity= 200
+// let minimumRequirement= 1000
+
+// console.log( stockQuantity >= minimumRequirement)
+
+// let data = "5000"
+// let dataType = 5000
+
+// console.log(dataType == data)
+
+// console.log(dataType === data)
+// The strict equality checks for both the value and data type while loose equality only checks for the value
+
+let supplierName = "NGH";
+let costUGX = 20000;
+// let record = supplierName.length >= 2 && costUGX >= 10000;
+
+let record = supplierName
+
+console.log(num1 + num2)
